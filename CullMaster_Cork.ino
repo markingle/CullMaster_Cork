4734e628cf4635ce12b626b5c3f553e07b7d814b
Mark Ingle <mark.ingle@omporium.com>
2022-02-19 19:21:09 -0500

    Release for field testing

--- CullMaster_Cork.ino
+++ CullMaster_Cork.ino
@@ -12,7 +12,7 @@
 #include <BLEDevice.h>
 #include <BLEServer.h>
 #include <BLEUtils.h>
-//#include <BLE2902.h>
+#include <BLE2902.h>
 //#include <BLE2904.h>
 #include <EEPROM.h>
 #include <TinyPICO.h>
@@ -42,7 +42,7 @@ uint32_t value = 0;
 //CORK5 = WHITE
 //CORK6 = BLUE
 
-// UID IN IPHONE APP
+// CORK Service UUID IN IPHONE APP
 //let RED_Service_UUID = CBUUID(string: "4fafc201-1fb5-459e-8fcc-c5c9c331914c")
 //let GREEN_Service_UUID = CBUUID(string: "4fafc201-1fb5-459e-8fcc-c5c9c331914d")
 //let BLACK_Service_UUID = CBUUID(string: "4fafc201-1fb5-459e-8fcc-c5c9c331914e")
@@ -50,7 +50,7 @@ uint32_t value = 0;
 //let WHITE_Service_UUID = CBUUID(string: "4fafc201-1fb5-459e-8fcc-c5c9c3319141")
 //let BLUE_Service_UUID = CBUUID(string: "4fafc201-1fb5-459e-8fcc-c5c9c3319142")
 
-//CORK CHARACTERISTICS DEFINED IN IPHONE APP
+//CORK FLASHRGB CHARACTERISTICS DEFINED IN IPHONE APP
 //let RED_FlashRGB_Characteristic_CBUUID = CBUUID(string: "BEB5483E-36E1-4688-B7F6-EA07361B2611")
 //let GREEN_FlashRGB_Characteristic_CBUUID = CBUUID(string: "BEB5483E-36E1-4688-B7F6-EA07361B2612")
 //let BLACK_FlashRGB_Characteristic_CBUUID = CBUUID(string: "BEB5483E-36E1-4688-B7F6-EA07361B2613")
@@ -58,10 +58,18 @@ uint32_t value = 0;
 //let WHITE_FlashRGB_Characteristic_CBUUID = CBUUID(string: "BEB5483E-36E1-4688-B7F6-EA07361B2615")
 //let BLUE_FlashRGB_Characteristic_CBUUID = CBUUID(string: "BEB5483E-36E1-4688-B7F6-EA07361B2616")
 
-#define CORK_SERVICE_UUID  "4fafc201-1fb5-459e-8fcc-c5c9c331914c"                // << SERVICE CHANGE HERE.....Weight_Scale_Service_CBUUID in CullMaster_V2 Xcode project
+#define CORK_SERVICE_UUID  "4fafc201-1fb5-459e-8fcc-c5c9c331914e"                // << SERVICE CHANGE HERE.....Cork_Service_CBUUID in CullMaster_V2 Xcode project
 #define FISH_WEIGHT_CHARACTERISTIC_B_UUID "beb5483e-36e1-4688-b7f7-ea07361b26c8" //Fish_Weight_Characteristic_CBUUID
-#define BATTERY_CHARACTERISTIC_C_UUID "beb5483e-36e1-4688-b7f8-ea07361b26d9"     // << BATTERY CHANGE HEREBattery_Characteristic_CBUUID
-#define FLASHRGB_CHARACTERISTIC_D_UUID "BEB5483E-36E1-4688-B7F6-EA07361B2611"    // << FLASHING RGB CHANGE HERE......FlashRGB_Characteristic_CBUUID
+//#define BATTERY_CHARACTERISTIC_C_UUID "beb5483e-36e1-4688-b7f8-ea07361b26d9"     // << BATTERY CHANGE HEREBattery_Characteristic_CBUUID
+
+//#define BATTERY_SERVICE_UUID "4fafc201-1fb5-459e-8fcc-c5c9c331915a"     // << BATTERY CHANGE HEREBattery_Characteristic_CBUUID
+//#define BATTERY_SERVICE_UUID BLEUUID((uint16_t)0x180F)
+
+//BLECharacteristic BatteryLevelCharacteristic(BLEUUID((uint16_t)0x2A19), BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_NOTIFY);
+//BLEDescriptor BatteryLevelDescriptor(BLEUUID((uint16_t)0x2901));
+
+
+#define FLASHRGB_CHARACTERISTIC_D_UUID "BEB5483E-36E1-4688-B7F6-EA07361B2613"    // << FLASHING RGB CHANGE HERE......FlashRGB_Characteristic_CBUUID
 
 #define PASSKEY 999999
 
@@ -182,39 +190,52 @@ void bleSecurity(){
   esp_ble_gap_set_security_param(ESP_BLE_SM_SET_RSP_KEY, &rsp_key, sizeof(uint8_t));
 };
 
+uint8_t level = 57;
 
 void setup() {
   Serial.begin(115200);
-  Serial.println("Cull Master RED");   //<<<<< CHANGE HERE FOR LOADING EACH CORK
+  Serial.println("Cull Master BLACK");   //<<<<< CHANGE HERE FOR LOADING EACH CORK
 
   pinMode(LED, OUTPUT);
   digitalWrite(LED, LOW);
   
 
   // Create the BLE Device
-  BLEDevice::init("RED");        //<<<<< CHANGE HERE FOR LOADING EACH CORK
+  BLEDevice::init("BLACK");        //<<<<< CHANGE HERE FOR LOADING EACH CORK
   //BLEDevice::setEncryptionLevel(ESP_BLE_SEC_ENCRYPT);
   //BLEDevice::setSecurityCallbacks(new SecurityCallback());
 
   // Create the BLE Server
-  pServer = BLEDevice::createServer();
+  BLEServer *pServer = BLEDevice::createServer();
   pServer->setCallbacks(new MyServerCallbacks());
+  
+  // Create the BLE Battery Service
+  //BLEService *pBatteryService = pServer->createService(BATTERY_SERVICE_UUID);
 
-  // Create the BLE Service
-  BLEService *pService = pServer->createService(CORK_SERVICE_UUID);
+  //pBatteryService->addCharacteristic(&BatteryLevelCharacteristic);
+  //BatteryLevelDescriptor.setValue("Percentage 0 - 100");
+  //BatteryLevelCharacteristic.addDescriptor(&BatteryLevelDescriptor);
+  //BatteryLevelCharacteristic.addDescriptor(new BLE2902());
 
+  //pAdvertising->addServiceUUID(BATTERY_SERVICE_UUID);
+  //pServer->getAdvertising()->addServiceUUID(BATTERY_SERVICE_UUID);
+
+  //pBatteryService->start();
+
+  // Create the BLE Cork Service
+  BLEService *pCorkService = pServer->createService(CORK_SERVICE_UUID);
 
   // Create a BLE Characteristic C
-  pCharacteristicC = pService->createCharacteristic(
-                      BATTERY_CHARACTERISTIC_C_UUID,
+  pCharacteristicB = pCorkService->createCharacteristic(
+                      FISH_WEIGHT_CHARACTERISTIC_B_UUID,
                       BLECharacteristic::PROPERTY_READ   |
                       BLECharacteristic::PROPERTY_WRITE_NR |
                       BLECharacteristic::PROPERTY_NOTIFY |
                       BLECharacteristic::PROPERTY_INDICATE
                     );
                     
-// Create a BLE Characteristic D
-  pCharacteristicD = pService->createCharacteristic(
+  //Create a BLE Characteristic D
+  pCharacteristicD = pCorkService->createCharacteristic(
                       FLASHRGB_CHARACTERISTIC_D_UUID,
                       BLECharacteristic::PROPERTY_READ   |
                       BLECharacteristic::PROPERTY_WRITE_NR  |
@@ -222,10 +243,12 @@ void setup() {
                       BLECharacteristic::PROPERTY_INDICATE
                     );
 
-  pCharacteristicC->setCallbacks(new MyCallbacks());
+  pCharacteristicB->setCallbacks(new MyCallbacks());
   pCharacteristicD->setCallbacks(new flashRGBCallback());
 
-  pService->start();
+  pServer->getAdvertising()->addServiceUUID(CORK_SERVICE_UUID);
+
+  pCorkService->start();
 
   // Start advertising
   BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
@@ -233,9 +256,10 @@ void setup() {
   pAdvertising->setScanResponse(false);
   pAdvertising->setMinPreferred(0x0);  // set value to 0x00 to not advertise this parameter
   BLEDevice::startAdvertising();
+  //pServer->getAdvertising()->start();
   Serial.println("Waiting a client connection to notify...");
 
-  Serial.println("Initializing RED");          //<<<<< CHANGE HERE FOR LOADING EACH CORK
+  Serial.println("Initializing BLACK");          //<<<<< CHANGE HERE FOR LOADING EACH CORK
 
   tp.DotStar_Clear();
   tp.DotStar_SetBrightness(255);
@@ -259,9 +283,22 @@ void loop() {
           tp.DotStar_SetPixelColor(0,0,255);
         }
         initBoot = false;
-        Serial.print("Battery Volage - ");
-        Serial.println(tp.GetBatteryVoltage());
-        Serial.println();
+        //Serial.print("Battery Volage - ");
+        //Serial.println(tp.GetBatteryVoltage());
+        //Serial.println();
+
+        //float voltage = tp.GetBatteryVoltage();
+        //float volt_temp = voltage/4.30;
+        //uint8_t voltage_percentage = 100*volt_temp;
+
+        //Serial.print("Battery Percentage - ");
+        //Serial.println(voltage_percentage);
+
+        //BatteryLevelCharacteristic.setValue(&voltage_percentage, 1);
+        //BatteryLevelCharacteristic.notify();
+        //delay(1000);
+
+        //level++;
         
         if (tp.IsChargingBattery()) {
           Serial.println("Battery is Charging");
